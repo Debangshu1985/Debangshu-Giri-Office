fb415a7946fb834e75d433539f30bffebae82ba1
Debangshu Giri <110330554+Debangshu1985@users.noreply.github.com>
2022-08-05 09:24:56 +0530

    This is my 2nd project
    about shoes size

---
+++ 2nd book marks.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd 
+
+
+# In[2]:
+
+
+pd.read_excel(r"C:\Users\user\Desktop\shoes size.xlsx")
+
+
+# In[3]:
+
+
+pd.read_csv(r"C:\Users\user\Desktop\shoes size - Copy.csv")
+
+
+# In[5]:
+
+
+pd.read_csv(r"C:\Users\user\Desktop\vehicle survey tsv.txt")
+
+
+# In[ ]:
+
+
+
+
