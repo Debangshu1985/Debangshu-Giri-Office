578d267d10e956cf652828c657e206b1c505b094
Debangshu Giri <110330554+Debangshu1985@users.noreply.github.com>
2022-08-11 20:27:17 +0530

    Add files via upload

---
+++ 4th data.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+f=72
+e=8
+z=72/8*4
+print(z)
+
+
+# In[2]:
+
+
+import os
+
+
+# In[3]:
+
+
+os.getcwd()
+
+
+# In[4]:
+
+
+import pandas as pd
+
+
+# In[5]:
+
+
+pd.read_excel("data.xlsx")
+
+
+# In[7]:
+
+
+pd.read_csv("data.tsv",sep="\t")
+
+
+# In[8]:
+
+
+pd.read_csv("data.tsv")
+
+
+# In[ ]:
+
+
+
+
